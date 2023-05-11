1ab4c2cd7d7c50f40558602c2513cb3ab808934a
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-05-11 09:17:12 +0700

    membuat Fragment

---
+++ app/src/main/java/com/example/projrctapp/OkeActivity2.java
@@ -0,0 +1,14 @@
+package com.example.projrctapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OkeActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oke2);
+    }
+}
